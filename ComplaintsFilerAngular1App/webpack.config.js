071c1b2ac39702d65ed377e2c3f19668ee15feac
M.Danish Farman <danish.farman@gmail.com>
2017-05-01 17:03:35 +0500

    -update - added separate client and serve apps structure

--- ComplaintsFilerAngular1App/webpack.config.js
+++ ComplaintsFilerAngular1App/webpack.config.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
-var webpack = require('webpack');
+var webpack = require('webpack')
 
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var jsFolderName = 'build/';
-var htmlPath = jsFolderName + '/';
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var jsFolderName = 'build/'
+var htmlPath = jsFolderName + '/'
 
 module.exports = {
-    context: path.join(__dirname, '/app'),
+    context: path.join(__dirname, '/ComplaintsFilerAngular1App'),
     entry: {
         app: [
             'webpack/hot/dev-server',
@@ -22,23 +22,22 @@ module.exports = {
     },
     module: {
         loaders: [{
-            test: /\.css$/,
-            loader: 'style-loader!css-loader'
-        },
-        {
-            test: /\.js$/,
-            loader: 'ng-annotate',
-            exclude: /node_modules/
-        },
-        {
-            test: /\.html$/,
-            loader: 'html'
-        },
-            ,
-        {
-            test: /\.(png|jpe?g|gif|woff|woff2|eot|ttf|svg)(\?[\s\S]+)?$/,
-            loader: 'file?name=[name].[ext]'
-        }
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'ng-annotate',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            }, ,
+            {
+                test: /\.(png|jpe?g|gif|woff|woff2|eot|ttf|svg)(\?[\s\S]+)?$/,
+                loader: 'file?name=[name].[ext]'
+            }
         ]
     },
     resolve: {
@@ -47,15 +46,15 @@ module.exports = {
             'utils': path.resolve(__dirname, './utils.dev')
         }
     },
-    plugins: [        
+    plugins: [
         new webpack.ProvidePlugin({ 'utils': 'utils' }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.ProvidePlugin({ '_': 'lodash', $: "jQuery", jQuery: "jQuery" }),
         // definePlugin takes raw strings and inserts them, so you can put strings of JS if you want.
         new webpack.DefinePlugin({ __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')) }),
         new HtmlWebpackPlugin({ path: path.join(__dirname, htmlPath), filename: './index.html', template: './index.html' }),
-        new webpack.optimize.UglifyJsPlugin({              
-            minimize: true, 
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
             compress: false,
             mangle: true
         })
@@ -63,4 +62,4 @@ module.exports = {
     devServer: {
         port: 1885
     }
-};
+}
--- ComplaintsFilerIonic2App/src/app/app.module.ts
+++ ComplaintsFilerIonic2App/src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { Camera } from '@ionic-native/camera';
+import { HomePage } from '../pages/home/home';
+import { MyApp } from './app.component';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+class CameraMock extends Camera {
+  getPicture(options){
+    return new Promise( (resolve, reject) => {
+      resolve(`TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=`);
+    });
+  }
+}
 
 @NgModule({
   declarations: [
@@ -22,9 +31,11 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    Camera,
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    {provide: Camera, useClass: CameraMock}
   ]
 })
 export class AppModule {}
